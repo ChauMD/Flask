78c8476ac7be28465f859baa2ead9f299ade9574
ChauMD <chauhoangleminh@gmail.com>
2022-07-06 03:59:38 +0700

    tweak postgreg

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,8 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] =  '123'
-    app.config['SQLALCHEMY_DATABASE_URI']= f'sqlite:///{DB_NAME}'
+#app.config['SQLALCHEMY_DATABASE_URI']= f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI']= f'postgres://lpkmfjwayhvoka:bae127a5b9629a5d7d1c8fe9934cae2dfb176c5830106b309862680158cec66b@ec2-54-211-255-161.compute-1.amazonaws.com:5432/deoei8ls1t9bai'
     db.init_app(app)
 
     from .views import views
